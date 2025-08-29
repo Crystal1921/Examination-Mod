d1916ae321aefd7bfc8776611a6765a2916b914e
NotPureRuby <2645305517@qq.com>
2025-08-29 15:35:35 +0800

    使用网络包来处理效果

--- src/main/java/com/mcg/examinationmod/event/DungeonBlockEvents.java
+++ src/main/java/com/mcg/examinationmod/event/DungeonBlockEvents.java
@@ -1,5 +1,6 @@
 package com.mcg.examinationmod.event;
 
+import com.mcg.examinationmod.network.PlatformRequestPacket;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
@@ -11,37 +12,33 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
 import static com.mcg.examinationmod.ExaminationMod.*;
 @EventBusSubscriber
 // 副本方块客户端事件
-public class DungeonBlockServerEvents {
+public class DungeonBlockEvents {
     @SubscribeEvent
-    public static void DungeonBlockServerEvent(PlayerInteractEvent.RightClickBlock event) {
-        //TODO : 这里可以直接只监听服务端事件，然后直接处理，不需要再客户端发包给服务端了
-
+    public static void DungeonBlockEvent(PlayerInteractEvent.RightClickBlock event) {
         //TODO : 原版会触发两次，主手一次，副手一次，导致物品右击产生bug
+        //因为要避免指定物品右键的时候打开Gui，然后Gui只能在客户端打开，所以用了网络包来处理效果
         Level level = event.getLevel();
         Player player = event.getEntity();
         BlockPos pos = event.getPos();
         BlockState state = level.getBlockState(pos);
         ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
         state.getBlock();
-        if (!level.isClientSide && event.getHand() == InteractionHand.MAIN_HAND) {
+        if (level.isClientSide && event.getHand() == InteractionHand.MAIN_HAND) {
             if (state.is(DUNGEON_BLOCK))
                 if (stack.is(START_TEST_ITEM.get())) {
-                    PlatformGenerator.generatePlatformAbovePlayer(player);
+                    //客户端到服务端
+                    PacketDistributor.sendToServer(new PlatformRequestPacket("start"));
                 } else if (stack.is(PARTY_TEST_ITEM.get())){
-                    if (player.level() instanceof ServerLevel serverLevel) {
-                        PlayerTeleporter.clearTeleportedPlayers();
-                        BlockPos platformCenter = PlatformGenerator.generatePlatformAbovePlayer(player, false);
-                        // 传送所有玩家到平台
-                        PlayerTeleporter.teleportAllPlayersToPlatform(serverLevel, platformCenter);
-                    }
+                    PacketDistributor.sendToServer(new PlatformRequestPacket("party"));
                 } else if (stack.is(TEAM_TEST_ITEM.get())) {
-                    String playersList = PlayerTeleporter.getTeleportedPlayersAsString();
-                    player.sendSystemMessage(Component.literal("被传送的玩家名单:"));
-                    player.sendSystemMessage(Component.literal(playersList));
+                    PacketDistributor.sendToServer(new PlatformRequestPacket("team"));
+                } else {
+                    GuiOpenWrapper.DungeonBlockGui();
                 }
         }
     }
